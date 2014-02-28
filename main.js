960472c3d48e76f9acbb8467a794fc86e34a6bbe
Benjamin Landers <benjaminrlanders@gmail.com>
2014-02-27 17:05:21 -0800

    made suggestions prototypal

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 chrome.omnibox.onInputChanged.addListener(
   function(text, suggest) {
     console.log('inputChanged: ' + text);
-    console.log(getSuggestions(text,obj))
-    suggest(getSuggestions(text,obj));
+    console.log(obj.getSuggestions(text))
+    suggest(obj.getSuggestions(text));
   });
-obj = {
-  hello:1,
-  hii:{there:'cool'}
-}
+obj = new Command();
+obj.hii = {there:'cool'};
+obj.hello = 1;
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(
   function(text) {
--- suggestions.js
+++ suggestions.js
@@ -1,45 +1,64 @@
-function getKeys(obj,match) {
-    var r = [];
-    for (var k in obj) {
-        if (!obj.hasOwnProperty(k)) 
-            continue;
-        if(k.startsWith(match))
-          r.push(k);
-    }
-    return r;
+function Command(){
 }
-function makeSuggestions(keys,obj,prefix,dprefix){
+Command.prototype.getKeys = function(match){
+  temp = this;
+  if(typeof match === 'object'){
+    for(var i = 0; i < match.length-1;i++){
+      temp = temp[match[i]];
+      console.log(temp);
+      if(typeof temp !== 'object'){
+        return undefined;
+      }
+    }
+    match = match[match.length-1];
+  }
   var r = [];
+  for (var k in temp) {
+      if (!temp.hasOwnProperty(k)) 
+          continue;
+      if(k.startsWith(match))
+        r.push(k);
+  }
+  return r;
+};
+Command.prototype.getValue = function(match){
+  for(var i = 0; i < match.length-1;i++){
+    temp = temp[match[i]];
+    console.log(temp);
+    if(typeof temp !== 'object'){
+      return undefined;
+    }
+  }
+  return temp[match[match.length-1]];
+};
+Command.prototype.getSuggestions = function(text){
+  var textArr = text.split(/\s+/);
+  
+  var temp = textArr[textArr.length-1];
+  textArr[textArr.length-1] = ''; 
+  var prefix = textArr.join(' '); //makes prefix
+  textArr[textArr.length-1] = temp;
+  
+  var a = makeSuggestions(this.getKeys(textArr),prefix);
+  if(a===undefined){
+    return [{content: text+' ',description: 'No suggestions'}];
+  }
+  textArr[textArr.length-1] = a[0].description;
+  temp = this.getValue(textArr);
+  if(a.length === 1&&temp&&typeof temp === 'object'){
+    var com = a[0];
+    textArr.push('');
+    a = makeSuggestions(this.getKeys(textArr),prefix+' '+com.description,com.description+' ');
+    a.splice(0,0,com);
+  }
+  return a;
+};
+function makeSuggestions(keys,prefix,dprefix){
+  var r = [];
+  if(!keys)return undefined;
   dprefix = dprefix || '';
   for(var i = 0;i < keys.length;i++){
     r.push({content:prefix+' '+keys[i],description:dprefix + keys[i]});
   }
   return r;
-}
-function getSuggestions(text,obj){
-  var textArr = text.split(/\s+/);
-  
-  var temp = textArr[textArr.length-1];
-  textArr[textArr.length-1] = ''; 
-  var prefix = textArr.join(' ') //makes prefix
-  textArr[textArr.length-1] = temp;
-  
-  temp = obj;
-  for(var i = 0; i < textArr.length-1;i++){
-    temp = temp[textArr[i]];
-    console.log(temp);
-    if(typeof temp !== 'object'){
-      console.log('RUN')
-      return [{content: text+' ',description: 'No suggestions'}]
-    }
-  }
-  var a = makeSuggestions(getKeys(temp,textArr[textArr.length-1]),temp,prefix);
-  if(a.length === 1&&temp[a[0].description]){
-    console.log('good')
-    var com = a[0];
-    
-    a = makeSuggestions(getKeys(temp[a[0].description],''),temp[a[0].description],prefix + ' '+a[0].description, a[0].description+' ');
-    a.splice(0,0,com);
-  }
-  return a;
 }
